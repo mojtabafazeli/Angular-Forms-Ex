71e339a188b16070f0c3c1490ed62df1d772910c
mojfazel <fazeli.mojtaba@gmail.com>
2020-06-07 19:19:37 -0400

    template-driven form created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    @ViewChild ('f')  signUpForm: NgForm
+    defaultQuestion = 'teacher';
+    answer = ""
+    genders = ['male', 'female']
+    user = {
+      username: '',
+      email: '',
+      secretQuestion: '',
+      answer: '',
+      gender: '',
+    }
+    submitted:boolean = false; 
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signUpForm.form.patchValue({ userData: 
+      { username: suggestedName}
+    })
+  }
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    console.log(this.signUpForm)
+    this.user.username=this.signUpForm.value.userData.username
+    this.user.email=this.signUpForm.value.userData.email
+    this.user.secretQuestion=this.signUpForm.value.secret
+    this.user.gender=this.signUpForm.value.gender
+
+    this.signUpForm.reset({
+      userData: {
+        username: 'Username',
+        email: 'Email'
+      },
+      gender: 'male'
+    });
+
   }
 }
